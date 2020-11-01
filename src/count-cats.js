bab5250d730cb95a0be2353309a26bcc51023a81
erfolen <dlababy@mail.ru>
2020-11-01 23:02:20 +0300

    fix: disaded two task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  if (matrix.length == 0) {
+    return 0 ;
+  } 
+  //let arrMat = matrix.flat(3);
+  let arrMat = matrix.reduce((acc, val) => acc.concat(val));
+  let arrCats = arrMat.filter(cat => cat == '^^');
+  return arrCats.length;
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam( members) {
+  arrBuk =[];
+  if (!Array.isArray(members)) {
+    return false;
+  } else if (members.length == 0){
+    return false;
+  }
+    for (let i = 0;i < members.length;i++) {
+      let buk = members[i];
+      if (!(typeof members[i] === 'string') || buk == '') {
+         continue;
+      } else {
+        buk = buk.trim();
+      }
+    arrBuk.push(buk[0].toUpperCase());
+   }
+   return arrBuk.sort().join("");
 };
