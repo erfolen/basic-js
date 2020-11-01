9a91f8c7de106c595862b92c73b3a9a0c54fb920
erfolen <dlababy@mail.ru>
2020-11-01 23:39:13 +0300

    fix: desaded three task (-two prover)

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  let monthMy = date.getMonth();
+    if (monthMy < 2 || monthMy == 11) {
+    return 'winter';
+  } else if (monthMy > 1 && monthMy < 5) {
+    return 'spring';
+  } else if (monthMy > 4 && monthMy < 8) {
+    return 'summer';
+  } else if (monthMy > 7 && monthMy < 11) {
+    return 'autumn';
+  } else {
+    return 'Unable to determine the time of year!';
+  }
 };
